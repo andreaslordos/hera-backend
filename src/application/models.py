fa57836fc7e4cd7ca9c81d84807f0ae6f26e5392
Andreas Lordos <andreaslordos@fb.com>
2022-07-29 01:05:18 -0700

    add model and generate access token

--- src/application/models.py
+++ src/application/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Backup(models.Model):
+    # generate unique access token upon instantiation of the model
+    accessToken = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    data = models.TextField()
--- src/application/views.py
+++ src/application/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from application.models import Backup
+import json
 
-# Create your views here.
+@csrf_exempt # be able to receive request even if there is no CSRF token
+def index(request):
+    if (request.method == 'GET'):
+        user = Backup.objects.create()
+        user.save()
+        return HttpResponse(json.dumps({ #send message back
+                'accessToken': user.accessToken.int,
+                'data': "roLzT3GBhVQw22WrUPAdsw==",   
+            }), 'application/json')
